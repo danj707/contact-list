c06e0d00fe7d18fc4377c62a7392600306496234
Dan Jenner <danjenner@gmail.com>
2016-08-01 08:52:37 -0500

    Still broken

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 //contact object, contains all contact information, including two arrays for
 //addresses and phone numbers linked to contact
 
-
 var contactObj = {
     firstname: '',
     lastname: '',
@@ -14,6 +13,7 @@ var contactObj = {
 
     //assigns the form data
     assign: function (data) {
+
         this.firstname = data[0].value;
         this.lastname = data[1].value;
         this.address = data[2].value + data[3].value + data[4].value;
@@ -23,23 +23,29 @@ var contactObj = {
     },
 
     //displays the clicked contact
-    display: function () {
-        for (var key in this) {
-            var txt = "<li> " + key + " : " + this[key] + "</li>";
-            $("ul.c_display").append(txt);
+    display: function (contact) {
+        if (contact = newContact.lastname) {
             $('ul.c_display').empty();
-            console.log(key + " = " + this[key]);
+            for (var key in newContact) {
+                // console.log("match! id: " + contact + "=" + newContact.lastname);
+                var txt = "<li> " + key + " : " + this[key] + "</li>";
+                $("ul.c_display").append(txt);
+            }
+        } else {
+            alert("no such match!");
         }
     },
 
-    //lists the contacts
     list: function () {
         $('div.contact_list').show();
-        var contactName = "<li><a class=\"contact_list\" href=\"#\">" + this.firstname + " " + this.lastname + "</a></li>";
+        var contactName = "<li><a class=\"contact_list\" id=" + this.lastname + " href=\"#\">" + this.firstname + " " + this.lastname + "</a></li>";
         $('ul.c_list').append(contactName);
-    }
+    },
+
 };
 
+//var newContact = new contactObj;
+var newContact = Object.create(contactObj); //obj constructor
 
 $(document).ready(function () {
     //on initial load, hide the contact list, as it's empty, and the right side nav
@@ -52,14 +58,14 @@ $(document).ready(function () {
     $("form#add_form").on("submit", function (event) {
         event.preventDefault(event);
         var data = $(this).serializeArray();
-        var newContact = Object.create(contactObj); //obj constructor
         newContact.assign(data);
         this.reset(); //reset the page form
     });
 
-    $(".contact_list").on("click", 'ul', newContact.display(event) {
+    $(".contact_list").on("click", 'ul', function (event) {
+        //event.preventDefault(event);
         $("div.contact_display").show();
-        newContact.display(); //<<<---- HERE IS WHAT DOESN'T WORK
+        var contact = newContact.display(String(event.target.id));
     });
 
 });
